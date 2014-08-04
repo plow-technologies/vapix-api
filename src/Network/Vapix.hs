dbf48af604e4317903af13f7c105089b74fb6ccc
Kevin Cotrone <kevincotrone@gmail.com>
2014-08-04 15:46:43 -0500

    Adding basic types for login info and recordings fix

--- src/Network/Vapix.hs
+++ src/Network/Vapix.hs
@@ -1,18 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Network.Vapix where
 
-import Network.Vapix.Internal
-import Data.Time.Clock
+import           Control.Applicative
+import           Control.Lens
+import           Data.ByteString
+import           Data.Time.Clock
+import           Network.Vapix.Internal
+import           Network.Vapix.Types
+import           Network.Wreq
+import           Text.XML.ToJSON
+import           Data.ByteString.Char8 as BSC
+--listRecordings :: String -> IO [VapixRecording]
+--listRecordings server = do
+--  res <- get server
+--  case res of
+--    (Success res) -> parseXML res
+--    (Error e) -> fail . show $ e
 
-data Recording = Recording {
-  recordingDisk :: String
-, recordingId :: String
-, recordingStart :: UTCTime
-, recordingEnd :: UTCTime
-, recordingType :: String
-} deriving (Eq)
+listRecordings :: AxisLogin -> String -> IO [VapixRecording]
+listRecordings login server = do
+  resp <- getWith opts server
+  views responseBody (\body -> (parseXML body) >>= (return . unVapixInfo)) resp
+  where opts = defaults & auth .~ fromLogin login
 
-listRecordings :: String -> [Recording]
-listRecordings server = undefined
 
-fetchRecording :: String -> Recording -> IO ()
-fetchRecording = undefined
+--buildRecordingURL :: AxisLogin
+
+fetchRecording :: AxisLogin ->  String -> VapixRecording -> IO [VapixRecording]
+fetchRecording login server r = do
+  resp <- getWith opts server
+  print resp
+  views responseBody (\body -> (parseXML body) >>= (return . unVapixInfo)) resp
+  where opts = defaults & auth .~ fromLogin login 
+
+fromLogin :: AxisLogin -> Maybe Auth
+fromLogin (AxisLogin user pass) = basicAuth (BSC.pack user) (BSC.pack pass)
+
+
+--asXML resp = resp ^? responseBody . parseXML
--- src/Network/Vapix/Types.hs
+++ src/Network/Vapix/Types.hs
@@ -1,9 +1,17 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, FlexibleInstances #-}
 module Network.Vapix.Types where
 
 import           Control.Applicative
 import           Data.Aeson
 import           Data.Time.Clock
+import Data.Time.Format
+import System.Locale
+import Data.Text
+
+data AxisLogin = AxisLogin {
+  axisUsername :: String
+, axisPassword :: String
+} deriving (Show, Eq)
 
 data VapixXML = VapixXML {
   vapixRoot :: VapixRecordingsWrapper
@@ -13,17 +21,41 @@ data VapixRecordingsWrapper = VapixRecordingsWrapper {
   vapixRecordingsWrapper :: VapixRecordings
 } deriving (Eq, Show)
 
-data VapixRecordings = VapixRecordings {
-  vapixRecordings :: [VapixRecording]
+data VapixRecordings = VapixRecordingsList { vapixRecordingList :: [VapixRecordingsAttr] } | VapixRecordingsSingle { vapixRecordingSingle :: VapixRecordingsAttr } deriving (Eq, Show)
+
+data VapixRecordingsAttr = VapixRecordingsAttr {
+  vapixRecordingsAttr :: VapixRecording
 } deriving (Eq, Show)
 
 data VapixRecording = VapixRecording {
   recordingDisk  :: String
 , recordingId    :: String
-, recordingStart :: UTCTime
-, recordingEnd   :: Maybe UTCTime
+, recordingStart :: VapixDate
+, recordingEnd   :: Maybe VapixDate
 , recordingType  :: String
 } deriving (Eq, Show)
+
+unVapixInfo :: VapixXML -> [VapixRecording]
+unVapixInfo = fmap vapixRecordingsAttr . unVapixRecordings . vapixRecordingsWrapper . vapixRoot
+
+unVapixRecordings :: VapixRecordings -> [VapixRecordingsAttr]
+unVapixRecordings (VapixRecordingsList xs) = xs
+unVapixRecordings (VapixRecordingsSingle x) = [x]
+
+newtype VapixDate = VapixDate { unVapixDate :: UTCTime } deriving (Eq, Show)
+
+instance FromJSON VapixDate where
+  parseJSON (String s) = case unpack s of
+                          "" -> fail "Unable to parse empty string"
+                          unS -> return $ VapixDate $ readTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S%QZ" unS
+  parseJSON _ = fail "Rule: Expecting VapixStartDate received other"
+
+instance FromJSON (Maybe VapixDate) where
+  parseJSON (String s) = case unpack s of
+                          "" -> return Nothing
+                          unS -> return . Just $ VapixDate $ readTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S%QZ" unS
+  parseJSON _ = fail "Rule: Expecting VapixStartDate received other"
+
 
 instance FromJSON VapixXML where
   parseJSON (Object tObj) = VapixXML <$> tObj .: "root"
@@ -33,16 +65,18 @@ instance FromJSON VapixRecordingsWrapper where
   parseJSON (Object tObj) = VapixRecordingsWrapper <$> tObj .: "recordings"
   parseJSON _ = fail "Rule: Expecting VapixRecordingsWrapper received other"
  
+instance FromJSON VapixRecordingsAttr where
+  parseJSON (Object tObj) = VapixRecordingsAttr <$> tObj .: "__attributes"
 
 instance FromJSON VapixRecordings where
-  parseJSON (Object tObj) = VapixRecordings <$> tObj .: "recording"
+  parseJSON (Object tObj) = (VapixRecordingsList <$> tObj .: "recording") <|> (VapixRecordingsSingle <$> tObj .: "recording")
   parseJSON _ = fail "Rule: Expecting VapixRecordings received other"
 
 
 instance FromJSON VapixRecording where
   parseJSON (Object tObj) = VapixRecording <$> tObj .: "diskid"
                                            <*> tObj .: "recordingid"
-                                           <*> tObj .: "starttimelocal"
-                                           <*> tObj .: "stoptimelocal"
+                                           <*> tObj .: "starttime"
+                                           <*> tObj .: "stoptime"
                                            <*> tObj .: "recordingtype"
   parseJSON _ = fail "Rule: Expecting VapixRecording received other"
