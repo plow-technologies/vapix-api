bd31c334896d9cce3f607dafde078536119c4321
Kevin Cotrone <kevincotrone@gmail.com>
2015-05-22 15:15:25 -0500

    Catching a parse error and return an empty list when it fails to parse

--- src/Network/Vapix.hs
+++ src/Network/Vapix.hs
@@ -7,17 +7,20 @@ import           Network.Vapix.Internal
 import           Network.Vapix.Types as Network.Vapix (AxisLogin(..), VapixRecording(..), unVapixInfo)
 import           Network.Wreq
 import           Text.XML.ToJSON
+import Control.Monad.Catch
 
 
 -- | list all current recordings on a camera/device
 listRecordings :: AxisLogin -> IO [VapixRecording]
 listRecordings login = do
   resp <- getWith opts url
-  views responseBody (\body -> (parseXML body) >>= (return . unVapixInfo)) resp
+  catch (views responseBody (\body -> (parseXML body) >>= (return . unVapixInfo)) resp) returnEmpty
   where opts = defaults & auth .~ (Just $ fromLogin login)
         url :: String
         url = exportURL $ addParams (buildURL urlHost "axis-cgi/record/list.cgi") [("startresultnumber", "0"), ("recordingid", "all")]
         urlHost = buildAxisHost login
+        returnEmpty :: JSONParseError -> IO [VapixRecording]
+        returnEmpty _ = return []
 
 -- | Build the URL for a recording in order to download it
 recordingURL :: AxisLogin -> VapixRecording -> String
